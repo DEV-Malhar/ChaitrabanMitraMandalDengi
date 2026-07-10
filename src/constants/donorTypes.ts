bf24cd220cdfd4c3863f2a7c0c09646fd75bd797
malhar <malhar@ziwotech.com>
2026-07-11 02:51:50 +0530

    Slight Changes for Labels

--- src/constants/donorTypes.ts
+++ src/constants/donorTypes.ts
@@ -1,6 +1,8 @@
 export const DONOR_TYPES = [
-  "Resident",
-  "Shopkeeper",
-  "Businessman",
-  "Politician"
+  "रहिवासी / Resident",
+  "भाडेकरू / Tenant",
+  "दुकानदार / Shopkeeper",
+  "व्यावसायिक / Businessman",
+  "राजकारणी / Politician",
+  "सामाजिक कार्यकर्ता / Social Worker",
 ];
